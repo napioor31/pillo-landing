8e6f72b63dc7e0c19e99b6bc4a8eef8a3f31e5c3
napioor <becreativ.studio@gmail.com>
2026-03-31 20:09:19 +0200

    analitics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import './i18n/index.js'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <HelmetProvider>
     <App />
+    <Analytics />
   </HelmetProvider>
 )
 
